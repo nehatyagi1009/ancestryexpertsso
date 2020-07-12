dd1cd35340881734267dc5bd058c1134578c23c4
nehaTyagiEn1009 <neha.en1009@gmail.com>
2020-07-12 14:27:29 +0530

    sso project commit

---
+++ src/main/java/com/altimetrik/ee/demo/controller/ApiController.java
@@ -0,0 +1,11 @@
+package com.altimetrik.ee.demo.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @RequestMapping({ "/hello" })
+    public String firstPage() {
+        return "Hello World";
+    }
+}
